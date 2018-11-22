21022005fcafb6f085d94ac1d68dccb03fa3b64b
nachomanchis <45250247+nachomanchis@users.noreply.github.com>
2018-11-21 22:50:12 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,7 +4,13 @@ public class Tester {
 
 	public static void main(String[] args)
 	{
-		RoachMotel motel = null;
-		
+		RoomFactory factory = new RoomFactory();
+        RoachMotel motel = new RoachMotel();
+        
+        motel.roomallocator();
+        System.out.println(motel.printRoomsType());
+        System.out.println( motel.rooms.get(0).getPrice());
+        motel.rooms.get(0).getSpa();
+        System.out.println( motel.rooms.get(0).getPrice());
 	}
 }

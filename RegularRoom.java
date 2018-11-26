79a3db0f9c9dca63ac1da28e9ad2a09bb61e4c22
yurikong <jack.du.2013@gmail.com>
2018-11-26 10:59:17 -0800

    Room
    concrete objects of Room of decorator pattern.

---
+++ RegularRoom.java
@@ -0,0 +1,7 @@
+package ProjectDesignPatterns;
+
+public class RegularRoom extends Room {
+	private String description = "regular room";
+	public RegularRoom() {}
+	public int cost() { return 50; }
+}

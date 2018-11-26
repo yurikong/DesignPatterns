98e6a1c41e161fa3e168bc0e0a6dfd581d50b588
yurikong <jack.du.2013@gmail.com>
2018-11-26 11:01:13 -0800

    revert

--- RoachMotel.java
+++ RoachMotel.java
@@ -3,19 +3,49 @@ package ProjectDesignPatterns;
 import java.util.ArrayList;
 
 public class RoachMotel {
-
-	private RoachMotel() {
-		rooms = new ArrayList<RoomFactory>(capacity);
+    RoachMotel() {
+		rooms = new ArrayList<Room>(capacity);
 		vacant = true;
 	}
-	private static RoachMotel instance;
-	private static ArrayList<RoomFactory> rooms;
+    
+	private static RoachMotel first_instance;
+	public ArrayList<Room> rooms;
 	private static boolean vacant;
 	private static int capacity = 10;
 	public static RoachMotel getInstance() {
-		if(instance == null)
-			instance = new RoachMotel();
-		return instance;
+		if(first_instance == null)
+			first_instance = new RoachMotel();
+		return first_instance;
 	}
-	
+     
+        
+    public ArrayList roomallocator(){
+        RoomFactory room=new RoomFactory();
+        
+        for (int i=0; i<capacity-3;i++)
+            this.rooms.add(room.createRoom("regular"));
+        
+        for (int i=0; i<2;i++)
+            this.rooms.add(room.createRoom("deluxe")); 
+        
+        this.rooms.add(room.createRoom("suite"));
+        
+        for (int i=0;i<capacity;i++){
+            this.rooms.get(i).setRoom_number(i+100);
+        }
+        return this.rooms;
+        
+    }
+    
+    public String printRoomsType(){
+        return rooms.toString();
+    }
+    
+	public String isFull(){
+            if (rooms.size()==0)
+                return "No vancancy, please enroll the waitlist";
+            else
+                return "Vacancy";
+	}
+        
 }
--- RoomFactory.java
+++ RoomFactory.java
@@ -1,9 +1,16 @@
 package ProjectDesignPatterns;
 
 public class RoomFactory {
-
 	public Room createRoom(String type) {
-		Room room = null;
-		
+		Room newRoom = null;
+		if (type.equals("suite")){
+		    return new Suite();
+		}else if (type.equals("deluxe")){
+		    return new Deluxe();
+		}else if (type.equals("regular")){
+		    return new Regular();
+		}else
+		    return null;
 	}
 }
+

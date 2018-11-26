211f7f905d1244a8740cdb486a1f2c3aab775902
yurikong <jack.du.2013@gmail.com>
2018-11-26 11:40:06 -0800

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,7 +1,7 @@
 package ProjectDesignPatterns;
 
 public abstract class Room {
-	private String description = "unknown room";
+	String description = "unknown room";
 	public String getDescription() { return description; }
 	public abstract int cost();
 }

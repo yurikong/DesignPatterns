7ad948386eb754ae142812bf637daa97ebfcc94f
yurikong <jack.du.2013@gmail.com>
2018-11-28 12:00:07 -0800

    Update RoachMotel.java

--- RoachMotel.java
+++ RoachMotel.java
@@ -3,15 +3,18 @@ package ProjectDesignPatterns;
 import java.util.ArrayList;
 
 public class RoachMotel {
-	RoomFactory factory;
-	private RoachMotel() {
-		factory = new RoomFactory();
-	}
 	private static RoachMotel instance;
+	private static RoomFactory factory;
+	private static int capacity = 20;
+	private static ArrayList<Room> rooms;
+	private RoachMotel() {}
 	public static RoachMotel getInstance() {
 		synchronized(RoachMotel.class) {
-			if(instance == null)
+			if(instance == null) {
 				instance = new RoachMotel();
+				factory = new RoomFactory();
+				rooms = new ArrayList<>(capacity);
+			}
 		}
 		return instance;
 	}

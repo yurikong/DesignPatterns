dc9c17b7b4e3c1fec376c4c17823f94c81c2ab6d
yurikong <jack.du.2013@gmail.com>
2018-11-28 11:53:20 -0800

    Update RoomFactory.java

--- RoomFactory.java
+++ RoomFactory.java
@@ -1,9 +1,28 @@
 package ProjectDesignPatterns;
 
-public class RoomFactory {
+import java.util.ArrayList;
 
-	public Room createRoom(String type) {
+public class RoomFactory {
+	
+	public RoomFactory() {}
+	public Room createRoom(String type, ArrayList<String> amenities) {
 		Room room = null;
-		
+		if(type.equals("regular room"))
+			room = new RegularRoom();
+		else if(type.equals("deluxe room"))
+			room = new DeluxeRoom();
+		else if(type.equals("suite"))
+			room = new Suite();
+		else
+			return null;
+		if(amenities.contains("food bar"))
+			room = new FoodBar(room);
+		if(amenities.contains("spa"))
+			room = new Spa(room);
+		if(amenities.contains("food bar autorefill"))
+			room = new FoodBarAutorefill(room);
+		if(amenities.contains("spray resistant shower"))
+			room = new SprayResistantShower(room);
+		return room;
 	}
 }

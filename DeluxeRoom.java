ba37310374c2719c536c8689073b50b2db2cd620
yurikong <jack.du.2013@gmail.com>
2018-11-29 18:33:22 -0800

    Update DeluxeRoom.java

--- DeluxeRoom.java
+++ DeluxeRoom.java
@@ -1,6 +1,6 @@
 package ProjectDesignPatterns;
 
 public class DeluxeRoom extends Room {
-	public DeluxeRoom() { description = "deluxe room"; }
+	public DeluxeRoom() { description = "Deluxe"; }
 	public int cost() { return 75; }
 }

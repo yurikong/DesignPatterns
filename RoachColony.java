13374add775deb670f720825c1638f8db3d7f3ed
yurikong <jack.du.2013@gmail.com>
2018-11-29 18:38:37 -0800

    Create RoachColony.java

---
+++ RoachColony.java
@@ -0,0 +1,15 @@
+package ProjectDesignPatterns;
+
+public class RoachColony {
+
+	private String name;
+	private int population;
+	private double rate;
+	public RoachColony(String name, int population, double rate)
+	{
+		this.name = name;
+		this.population = population;
+		this.rate = rate;
+	}
+	
+}

ec1278e4b81fed628f9753896564078de5480fed
nachomanchis <45250247+nachomanchis@users.noreply.github.com>
2018-12-03 12:02:32 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,16 +1,13 @@
-package ProjectDesignPatterns;
-
-public class Tester {
-
-	public static void main(String[] args)
-	{
-		RoomFactory factory = new RoomFactory();
+RoomFactory factory = new RoomFactory();
         RoachMotel motel = new RoachMotel();
         
         motel.roomallocator();
         System.out.println(motel.printRoomsType());
         System.out.println( motel.rooms.get(0).getPrice());
-        motel.rooms.get(0).getSpa();
-        System.out.println( motel.rooms.get(0).getPrice());
-	}
-}
+        
+        Room room_1 = factory.createRoom("regular");
+        room_1.setRoom_number(100);
+        System.out.println(room_1.getDescription()+ " Price: "+ room_1.getPrice());
+        room_1 = new FoodBar(room_1);
+        System.out.println(room_1.getDescription()+" Price: "+ room_1.getPrice());
+        

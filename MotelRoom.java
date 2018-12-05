47ee06e15241baa02b010580b50b2438ed400f79
yurikong <jack.du.2013@gmail.com>
2018-12-05 12:01:24 -0800

    Revert "Update MotelRoom.java"
    This reverts commit a8dfbc7adef7ecfb19019409f02b16cc2a7c35f5.

--- MotelRoom.java
+++ MotelRoom.java
@@ -14,37 +14,31 @@
 package ProjectDesignPatterns;
 
 public abstract class MotelRoom {
-	//type refers to the room type such as "Deluxe"
+	
 	protected String type = "Unknown";
 	protected int roomNumber;
 	protected RoachColony guest;
-	//Returns the room type
 	public String getType() 
 	{
 		return type;
 	}
 	public abstract double cost();
-	//Room number setter
 	public void setRoomNumber(int roomNumber) 
 	{
 		this.roomNumber = roomNumber;
 	}
-	//Room number getter
 	public int getRoomNumber() 
 	{
 		return roomNumber; 
 	}
-	//Guest setter
 	public void setGuest(RoachColony rc) 
 	{
 		guest = rc; 
 	}
-	//Guest getter
 	public RoachColony getGuest() 
 	{
 		return guest; 
 	}
-	//Returns the room type and its cost
 	public String toString() 
 	{
 		return getType() + " " + cost(); 
